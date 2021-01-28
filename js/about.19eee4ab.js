bd944c5c8a7040c2c2fa2abbc2a2908fb499329b
marionstaats <70648703+marionstaats@users.noreply.github.com>
2021-01-28 11:47:18 +0100

    deploy

---
+++ js/about.19eee4ab.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is the todo list app made in Vue")])])}],i=n("2877"),u={},l=Object(i["a"])(u,s,a,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.19eee4ab.js.map
